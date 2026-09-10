28491f629fc39b8d51334cd25f0207fdc0ae4d51
Daniel Ospina <ingedanielospina@gmail.com>
2026-09-10 09:43:39 -0500

    chore_bumped_version_to_1_2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "io.github.daniospina9.cuentaconmigo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
